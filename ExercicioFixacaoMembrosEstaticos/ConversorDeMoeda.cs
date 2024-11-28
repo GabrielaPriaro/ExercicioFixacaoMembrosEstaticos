66c268806cc2c4e2e20b7bd82494b7f320b86c0f
Gabriela <gabriela.priaro@gmail.com>
2024-11-28 14:17:13 +0000

    Add project files.

---
+++ ExercicioFixacaoMembrosEstaticos/ConversorDeMoeda.cs
@@ -0,0 +1,13 @@
+﻿namespace ExercicioFixacaoMembrosEstaticos
+{
+    internal static class ConversorDeMoeda
+    {
+        public static double IOF = 6.0 / 100;
+
+        public static double Converter(double precoDolar, double valorCompra)
+        {
+            var valorConvertido = precoDolar * valorCompra;
+            return valorConvertido + (IOF * valorConvertido);
+        }
+    }
+}
---
+++ ExercicioFixacaoMembrosEstaticos/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace ExercicioFixacaoMembrosEstaticos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual a cotaçao do dolar? ");
+            double precoDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            Console.Write("Qual o valor em dolar vai comprar? ");
+            double valorCompra = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double valorCompraCalculada = ConversorDeMoeda.Converter(precoDolar, valorCompra);
+
+            Console.WriteLine($"Valor a ser pago em reais = {valorCompraCalculada.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
